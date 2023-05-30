f4c8c1310f075f4c1e8cc55767c975fba4da536d
MariRug10 <marianoruggeri0403@gmail.com>
2023-05-30 00:39:59 -0300

    Clases!

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,7 +1,18 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.modelo.Message;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        App.main(args);
+        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido por favor ingrese su nombre (6 Caracteres minimo): ");
+        
+        String nombre = scanner.nextLine();
+
+        System.out.println("El nombre ingresado es:"+nombre);
+App.main(args);
     }
 }
