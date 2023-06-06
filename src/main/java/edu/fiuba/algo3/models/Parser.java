23ef5af6a36f4c648da69b2a35bdceeb93137529
manuperez10 <manumanuel-24@hotmail.com>
2023-06-06 02:30:39 -0300

    Se agrego la libreria json

--- src/main/java/edu/fiuba/algo3/models/Parser.java
+++ src/main/java/edu/fiuba/algo3/models/Parser.java
@@ -1,34 +1,57 @@
 package edu.fiuba.algo3.models;
-/* 
-import org.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json;*/
+
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Map;
+import org.json.JSONObject;
 
-/* 
+
+
 public class Parser {
 
-    public static void parser(){
+    public static void parser() {
 
+        String filePath = "ruta_del_archivo.json";
+
+        BufferedReader reader = null;
         try {
-            // Lee el archivo JSON
-            BufferedReader br = new BufferedReader(new FileReader("ruta/al/archivo.json"));
-            // Convierte el archivo JSON a un objeto Java
-            Gson gson = new Gson();
-            Mapa mapa = gson.fromJson(br, Mapa.class);
-            
-            // Accede a los datos del mapa
-            Map<String, String[]> mapaCompleto = mapa.getMapa();
-            String[] fila1 = mapaCompleto.get("1");
-            // ...continúa accediendo a los demás datos según necesites
+            // Abrir el archivo
+            reader = new BufferedReader(new FileReader(filePath));
 
-            // Cierra el BufferedReader
-            br.close();
-        } catch (Exception e) {
+            String line;
+            StringBuilder fileContent = new StringBuilder();
+
+            // Leer y almacenar cada línea del archivo
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line);
+            }
+
+            // Crear un objeto JSON a partir del contenido del archivo
+            JSONObject jsonObject = new JSONObject(fileContent.toString());
+
+            // Hacer algo con el objeto JSON, por ejemplo, imprimirlo
+            System.out.println(jsonObject.toString());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    // Cerrar el archivo
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-}}*/
+
+    }
+}
+
 
