658e71bedfbd2caf110a2c52ceab48e41fad02e5
Julio Bernardino Pinango Baez <julio.pinango@kyndryl.com>
2023-06-14 19:24:52 -0300

    Parser listo (solo la logica, falta probarlo) y la incializacion de mapa (tambien solo la logica, falta probarlo) y falta hacer un refactor de TOOODAS las pruebas que utilicen el parser y el mapa

--- src/main/java/edu/fiuba/algo3/models/Mapa.java
+++ src/main/java/edu/fiuba/algo3/models/Mapa.java
@@ -1,14 +1,19 @@
 package edu.fiuba.algo3.models;
 
 import edu.fiuba.algo3.models.Enemigos.Enemigo;
+import edu.fiuba.algo3.models.Enemigos.EnemigoFactory;
 import edu.fiuba.algo3.models.Parcelas.Parcela;
+import edu.fiuba.algo3.models.Parcelas.ParcelaFactory;
+import edu.fiuba.algo3.models.Parcelas.Pasarela;
 import edu.fiuba.algo3.models.Defensas.Defensa;
 import edu.fiuba.algo3.models.Defensas.DefensaFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class Mapa
 {
@@ -18,12 +23,55 @@ public class Mapa
     private Map<Cordenada, Parcela> parcelas = new HashMap<>();
     private List<Enemigo> listaEnemigos=new ArrayList<>();
 
-
-
-
+    /*
     public Mapa(Map<Cordenada, Parcela> parcelas2 , List<List<Enemigo>> spawn) {
         parcelas=parcelas2;
         Spawn=spawn;
+    }
+    */
+    public Mapa(String rutaArchivo) {
+        Parser parser= new Parser();
+        this.parcelas=inicializarParcelas(parser.leerMapa(rutaArchivo));
+        this.Spawn=inicializarEnemigos(parser.desglosarEnemigos(rutaArchivo),parser.formarCamino(rutaArchivo));
+    }
+    private Map<Cordenada, Parcela>  inicializarParcelas(List<List<String>> mapa){
+        Map<Cordenada, Parcela> nuevoMapa= new HashMap<>();
+        for (int i = 0; i < mapa.size(); i++) {
+            List<String> columna=mapa.get(i);
+            for (int j = 0; j < columna.size(); j++) {
+                String parcela=columna.get(j);
+                Cordenada nuevaCordenada= new Cordenada(i, j);
+                Parcela nuevaParcela=ParcelaFactory.obtenerParcela(nuevaCordenada, parcela);
+                nuevoMapa.put(nuevaCordenada, nuevaParcela);
+            }
+        }
+        return nuevoMapa;
+    }
+    private Queue<Pasarela> inicializarCaminoDeEnemigos(List<List<String>>camino){
+        Queue<Pasarela> nuevoCamino=new LinkedList<>(); 
+        for (int i = 0; i < camino.size(); i++) {
+            List<String> unaPasarela=camino.get(i);
+                Cordenada nuevaCordenada= new Cordenada(Integer.parseInt(unaPasarela.get(1)),Integer.parseInt(unaPasarela.get(2)));
+                var nuevaPasarela=ParcelaFactory.obtenerParcela(nuevaCordenada, unaPasarela.get(0));
+                nuevoCamino.add((Pasarela) nuevaPasarela);
+        }
+        return nuevoCamino;
+
+    }
+    private List<List<Enemigo>>  inicializarEnemigos(List<List<String>> enemigos, List<List<String>> camino){
+         Queue<Pasarela> nuevoCamino=this.inicializarCaminoDeEnemigos(camino);
+        List<List<Enemigo>> Spawn =new ArrayList<>();
+        for (int i = 0; i < enemigos.size(); i++) {
+            List<String> enemigosDelTurno=enemigos.get(i);
+            List<Enemigo> nuevosEnemigos=new ArrayList<>();
+            for (int j = 0; j < enemigosDelTurno.size(); j++) {
+                String enemigo=enemigosDelTurno.get(j);
+                Enemigo nuevoEnemigo=EnemigoFactory.obtenerEnemigo(nuevoCamino,enemigo);
+                nuevosEnemigos.add(nuevoEnemigo);
+                Spawn.add(nuevosEnemigos);
+            }
+        }
+        return Spawn;
     }
 
     public boolean colocarDefensaEnEstaPosicion(int x,int y, String tipo){
--- src/main/java/edu/fiuba/algo3/models/Parser.java
+++ src/main/java/edu/fiuba/algo3/models/Parser.java
@@ -29,7 +29,100 @@ import edu.fiuba.algo3.models.Parcelas.Tierra;
 
 public class Parser {
 
-    public static List<List<Enemigo>> desglosarEnemigos(String rutaArchivo,Queue<Pasarela> camino) {
+    public List<List<String>> desglosarEnemigos(String rutaArchivo) {
+        List<List<String>> enemigosList = new ArrayList<>();
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(rutaArchivo)));
+            JSONArray jsonArray = new JSONArray(json);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                JSONObject enemigosObj = jsonObject.getJSONObject("enemigos");
+                List<String> enemigos = new ArrayList<>();
+
+                for (String enemigoKey : enemigosObj.keySet()) {
+                    int cantidad = enemigosObj.getInt(enemigoKey);
+                    for (int j = 0; j < cantidad; j++) {
+                        enemigos.add(enemigoKey);
+                    }
+                }
+                enemigosList.add(enemigos);
+            }
+
+            return enemigosList;
+        } catch (IOException e) {
+            // Manejar el error si ocurre una excepción de lectura del archivo
+            e.printStackTrace();
+            return null; // Devolver null solo en caso de error
+        }
+    }
+
+
+       public List<List<String>> leerMapa(String rutaArchivo) {
+
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(rutaArchivo)));
+            List<List<String>> mapa = new ArrayList<>(); // Lista de listas que almacenará el mapa
+
+            JSONObject jsonObject = new JSONObject(json); // Crear un objeto JSONObject a partir de la cadena JSON proporcionada
+            JSONObject mapaObj = jsonObject.getJSONObject("Mapa"); // Obtener el objeto JSON que representa el mapa
+
+            for (int i = 0; i < mapaObj.length(); i++) { // Recorrer cada elemento en el objeto del mapa
+                JSONArray filaArray = mapaObj.getJSONArray(String.valueOf(i + 1)); // Obtener el array JSON que representa una fila del mapa
+                List<String> fila = new ArrayList<>();
+
+                for (int j = 0; j < filaArray.length(); j++) {
+                    String elemento = filaArray.getString(j); // Obtener el elemento de la fila actual como una cadena
+                    fila.add(elemento);
+                }
+
+                mapa.add(fila);
+            }
+
+            return mapa; // Devolver el mapa como una lista de listas
+        } catch (IOException e) {
+            // Manejar el error si ocurre una excepción de lectura del archivo
+            e.printStackTrace();
+            return null; // Devolver null solo en caso de error
+        }
+    }
+
+    public List<List<String>> formarCamino(String rutaArchivo){
+        List<List<String>> camino = new ArrayList<>();
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(rutaArchivo)));
+            List<List<String>> mapa = new ArrayList<>(); // Lista de listas que almacenará el mapa
+
+            JSONObject jsonObject = new JSONObject(json); // Crear un objeto JSONObject a partir de la cadena JSON proporcionada
+            JSONObject mapaObj = jsonObject.getJSONObject("Mapa"); // Obtener el objeto JSON que representa el mapa
+            for (int i = 0; i < mapaObj.length(); i++) { // Recorrer cada elemento en el objeto del mapa
+                JSONArray filaArray = mapaObj.getJSONArray(String.valueOf(i + 1)); // Obtener el array JSON que representa una fila del mapa
+                List<String> pasarela = new ArrayList<>();
+                for (int j = 0; j < filaArray.length(); j++) {
+                    String elemento = filaArray.getString(j); // Obtener el elemento de la fila actual como una cadena
+                    if(elemento.equals("Pasarela")){
+                        pasarela.add(elemento);
+                        pasarela.add(String.valueOf(i-1));
+                        pasarela.add(String.valueOf(j));
+                        camino.add(pasarela);
+                    }
+                }
+            }
+
+            return camino; // Devolver el mapa como una lista de listas
+        } catch (IOException e) {
+            // Manejar el error si ocurre una excepción de lectura del archivo
+            e.printStackTrace();
+            return null; // Devolver null solo en caso de error
+        }
+    }
+
+
+
+
+
+    /*
+        public static List<List<Enemigo>> desglosarEnemigos(String rutaArchivo,Queue<Pasarela> camino) {
         try{
             
             String json=new String(Files.readAllBytes(Paths.get(rutaArchivo)));
@@ -133,13 +226,10 @@ public class Parser {
                 e.printStackTrace();
                 return null; // Devolver null solo en caso de error
             }
-
-
-
-
     }
+    
 
-
+     */
 
 
 
