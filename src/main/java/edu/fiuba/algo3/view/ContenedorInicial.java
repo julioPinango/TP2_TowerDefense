837746273b420882baffcba1eff033ab80c2a5e3
manuperez10 <manumanuel-24@hotmail.com>
2023-06-28 01:45:00 -0300

    tamaño y posicion cuadro de texto inicio; juego centrado

--- src/main/java/edu/fiuba/algo3/view/ContenedorInicial.java
+++ src/main/java/edu/fiuba/algo3/view/ContenedorInicial.java
@@ -8,12 +8,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Box;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -27,7 +23,7 @@ public class ContenedorInicial extends VBox {
         Image imagen = new Image("file:src/main/img/inicio.png");
         BackgroundImage imagenFondo = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true));
         this.setBackground(new Background(imagenFondo));
-        
+
 
         Label aviso = new Label("*El nombre debe tener por lo menos 6 caracteres");
         aviso.setFont(new Font("Trebuchet MS", 18));
@@ -38,8 +34,14 @@ public class ContenedorInicial extends VBox {
         label.setFont(new Font("Trebuchet MS", 18));
         label.setStyle("-fx-font-weight: bold; -fx-background-color: white;");
 
+
         TextField nombreJugador = new TextField();
         nombreJugador.setFont(new Font("Trebuchet MS", 18));
+        nombreJugador.setPrefWidth(650);
+
+
+        HBox b = new HBox(nombreJugador);
+        b.setAlignment(Pos.CENTER);
 
         Button buttonIniciar = new Button("Iniciar juego");
         buttonIniciar.setDisable(true);
@@ -52,11 +54,10 @@ public class ContenedorInicial extends VBox {
         nombreJugador.setOnKeyPressed(tx);
 
         this.getChildren().addAll(
-                //titulo,
                 label,
-                nombreJugador,
                 aviso,
-                buttonIniciar
+                buttonIniciar,
+                b
         );
 
         
